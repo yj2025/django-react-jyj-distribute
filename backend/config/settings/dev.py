39e1c31be61621ec2124e3e11ab9e5b965a8673a
Fellas <neuneu12@naver.com>
2025-06-20 11:09:30 +0900

    commit

--- backend/config/settings/dev.py
+++ backend/config/settings/dev.py
@@ -1,8 +1,8 @@
 # config/settings/dev.py
 #개발 전용 설정만 덮어쓴다"는 의미
-# from .base import *  
-# import os
-# from pathlib import Path
+from .base import *  
+import os
+from pathlib import Path
 
 # # 예시: 환경 변수 가져오기
 # SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
